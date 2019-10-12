c6e4fcbc55c228b99203962891cec6b3e9098c15
Tero Frondelius <tero.frondelius@gmail.com>
2019-10-12 14:51:05 +0300

    deps.jl to install libmises_umat

---
+++ deps/build.jl
@@ -0,0 +1,11 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/UMAT.jl/blob/master/LICENSE
+
+dependencies = [
+    "https://github.com/TeroFrondelius/umat_binaries_builder/releases/download/v0.1.0/build_umat_binaries.v0.1.0.jl"
+]
+
+for build_script in dependencies
+    script_name = split(build_script,"/")[end]
+    include(download(build_script,script_name))
+end
