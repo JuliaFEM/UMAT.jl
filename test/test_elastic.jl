d0e3983eaa82723a4051dc065ee16397e0a878f1
Tero Frondelius <tero.frondelius@gmail.com>
2019-10-13 17:23:10 +0300

    libelastic testing

--- test/test_elastic.jl
+++ test/test_elastic.jl
@@ -10,16 +10,14 @@ else
     lib_dir = joinpath(pkg_dir,"..","deps","usr","lib")
 end
 
-material = UmatMaterial(lib_path=joinpath(lib_dir,"libelastic." * dlext),
-                        behaviour=:umat_)
+material = UmatMaterial(lib_path=joinpath(lib_dir,"libelastic." * dlext))
 
-strains = [1.0e-3, 0.0, 0.0, 0.0, 0.0, 0.0]
+strains = [1.0e-3, 0.0, 0.0, 0.0]
+ref_stress = [282.6923171355886, 121.1538570784259, 121.1538570784259, 0.0]
 material.ddrivers = UmatDriverState(STRAN = strains)
 UMAT.integrate_material!(material)
 update_material!(material)
 
-
-@test isapprox(material.variables.STRESS[1],210.)
-for i in 2:6
-    @test isapprox(material.variables.STRESS[i],0.0)
+for i in 1:4
+    @test isapprox(material.variables.STRESS[i],ref_stress[i],atol=sqrt(eps()))
 end
